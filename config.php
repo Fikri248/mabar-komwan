cd8aa9478393bf75dbcf329bb1d9ee63a660222b
Fikri248 <fikriisfahani12@gmail.com>
2025-11-23 22:56:58 +0700

    second commit

--- config.php
+++ config.php
@@ -4,20 +4,13 @@ $username = "admin1234";
 $password = "ahong_1234";
 $database = "logistik_db";
 
-// Inisialisasi koneksi untuk Azure MySQL
 $conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$conn) {
-    die('mysqli_init gagal');
-}
-
-mysqli_real_connect($conn, $host, $username, $password, $database);
-
-// Cek koneksi
-if (mysqli_connect_errno()) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-// Set charset ke utf8mb4
 mysqli_set_charset($conn, "utf8mb4");
 ?>
